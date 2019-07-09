60d8fbfbe9bb8f030ae7c3c185dbba21707c28b5
Alexander Proschek <alexander.proschek@gmail.com>
2019-07-09 19:05:35 -0400

    Changed some more things

--- app.py
+++ app.py
@@ -8,4 +8,8 @@ load_dotenv('./.env')
 
 st = SpaceTrackClient(os.getenv('EMAIL'), os.getenv('PASSWD'))
 
-print(st.tle_latest(norad_cat_id=[25544, 41335], ordinal=1, format='tle'))
+output = st.tle_latest(norad_cat_id=[25544, 41335], ordinal=1, format='tle')
+
+file = open('output.tle', 'w')
+file.write(output)
+file.close()
